bc5ec54d8878c6afd8fc7ebf73eae992abcb0703
raidelo <raidelosix@gmail.com>
2026-05-13 12:42:23 -0400

    feat: add functionality to REPL

--- src/calculator_algorithm/__init__.py
+++ src/calculator_algorithm/__init__.py
@@ -1,3 +1,9 @@
+from decimal import DivisionByZero, InvalidOperation
+
+from calculator_algorithm.errors import InvalidOperator, InvalidTokenSequence
+from calculator_algorithm.utils import tokenize
+from calculator_algorithm.utils.recursive import resolve_operation, tokens_to_operation
+
 PROMPT = ">> "
 
 
@@ -7,5 +13,28 @@ def main() -> None:
 
         if inp.lower() in ["q", "quit", "exit"]:
             break
+
+        try:
+            tokens = tokenize(inp)
+        except InvalidOperation:
+            print(f"error: Invalid operation: {inp!r}")
+            return
+        except InvalidOperator as e:
+            print(f"error: Invalid operator: {e.args[0]!r}")
+            return
+
+        try:
+            operation = tokens_to_operation(tokens)
+        except InvalidTokenSequence:
+            print(f"error: Invalid expression: {inp!r}")
+            return
+
+        try:
+            solution = resolve_operation(operation)
+        except DivisionByZero:
+            print(f"error: Can't divide by zero: {inp!r}")
+            return
+
+        print(solution)
 
     print("\nExitting ...")
