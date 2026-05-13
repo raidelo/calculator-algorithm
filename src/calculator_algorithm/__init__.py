018f96bcb6976f14cd80282988dcbdcea13bcf78
raidelo <raidelosix@gmail.com>
2026-05-13 12:30:49 -0400

    feat: add basic REPL

--- src/calculator_algorithm/__init__.py
+++ src/calculator_algorithm/__init__.py
@@ -1,2 +1,11 @@
+PROMPT = ">> "
+
+
 def main() -> None:
-    print("Hello from calculator-algorithm!")
+    while True:
+        inp = input(PROMPT)
+
+        if inp.lower() in ["q", "quit", "exit"]:
+            break
+
+    print("\nExitting ...")
